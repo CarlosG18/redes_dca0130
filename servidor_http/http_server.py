83f7fb3776770cb28874196f69a0bdd2076449f3
CarlosG18 <c.gabriel.abc18@gmail.com>
2023-05-16 20:05:27 -0300

    create server http

--- servidor_http/http_server.py
+++ servidor_http/http_server.py
@@ -8,6 +8,7 @@
 
 # importacao das bibliotecas
 import socket
+import os
 
 # definicao do host e da porta do servidor
 HOST = '' # ip do servidor (em branco)
@@ -28,19 +29,68 @@ listen_socket.listen(1)
 # imprime que o servidor esta pronto para receber conexoes
 print ('Serving HTTP on port %s ...' % PORT)
 
+def file_content(file_path):
+  if os.path.exists(file_path):
+    file = open(file_path, "rb")
+    content = file.read()
+    return content.decode('utf-8'), True
+  else:
+    content = """
+<html>
+  <head></head>
+  <body>
+    <h1>404 Not Found</h1>
+  </body>
+</html>
+"""
+    return content, False
+
 while True:
     # aguarda por novas conexoes
     client_connection, client_address = listen_socket.accept()
     # o metodo .recv recebe os dados enviados por um cliente atraves do socket
     request = client_connection.recv(1024)
-    # imprime na tela o que o cliente enviou ao servidor
-    print (request.decode('utf-8'))
-    # declaracao da resposta do servidor
-    http_response = """\
-HTTP/1.1 200 OK
-
-Hello, World!
+    str_request = request.decode('utf-8')
+    print(str_request)
+    
+    message_ok = "HTTP/1.1 200 OK"
+    message_not_found = "HTTP/1.1 404 Not Found"
+    message_bad_request = "HTTP/1.1 400 Bad Request"
+    
+    x = str_request.split(" ")
+    
+    if x[0] == "GET":
+      path = "."+x[1]
+      if path == "./":
+        content, exists = file_content("./index.html")
+        if exists:
+          message_http = message_ok
+        else:
+          message_http = message_not_found
+      else:
+        content, exists = file_content(path)
+        if exists:
+          message_http = message_ok
+        else:
+          message_http = message_not_found
+    else:
+        message_http = message_bad_request
+        content = """
+<html>
+  <head></head>
+  <body>
+    <h1>400 Bad Resquest</h1>
+  </body>
+</html>
 """
+    
+    http_response = f"""\
+{message_http}
+
+{content}
+"""
+    
+    # declaracao da resposta do servidor
     # servidor retorna o que foi solicitado pelo cliente (neste caso a resposta e generica)
     client_connection.send(http_response.encode('utf-8'))
     # encerra a conexao
